57e94169e256d24f596c1e176de34d72df7e478e
Kaviraj <kavirajkanagaraj@gmail.com>
2020-12-29 15:59:09 +0100

    Adding stack machine. hoc4

--- hoc.h
+++ hoc.h
@@ -10,3 +10,17 @@ typedef struct Symbol {		/* symbol table entry  */
 
 Symbol *install();
 Symbol *lookup();
+
+typedef union Datum {		/* interpreter stack type */
+  double val;
+  Symbol *sym;
+} Datum;
+
+extern Datum pop();
+
+typedef void (*Inst)();		/* machine instruction */
+#define STOP (Inst) 0
+
+extern Inst prog[];
+extern void eval(), add(), sub(), mul(), divide(), negate(), power();
+extern void assign(), bltin(), varpush(), constpush(), print();
